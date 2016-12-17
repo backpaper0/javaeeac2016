10355ed9e957a735fc35a219ad955d9fd855dc4f
Taichi Uragami <backpaper0@gmail.com>
2016-12-18 08:55:52 +0900

    Change from Jersey OAuth 2 to Spring Security OAuth

--- helloworld-spring/src/main/java/com/example/HelloWorld.java
+++ helloworld-spring/src/main/java/com/example/HelloWorld.java
@@ -1,34 +1,16 @@
 package com.example;
 
-import java.io.StringReader;
+import java.security.Principal;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.ws.rs.client.Client;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @Value("${userUri}")
-    private String userUri;
-
-    @Autowired
-    private Client client;
-
     @GetMapping
-    public String home() {
-        String json = client
-                .target(userUri)
-                .request()
-                .get(String.class);
-        JsonObject root = Json.createReader(new StringReader(json)).readObject();
-        JsonObject principal = root.getJsonObject("principal");
-        String username = principal.getString("username");
+    public String home(Principal principal) {
+        String username = principal.getName();
         return String.format("Hello, %s!", username);
     }
 }
--- helloworld-spring/src/main/java/com/example/HelloWorldApplication.java
+++ helloworld-spring/src/main/java/com/example/HelloWorldApplication.java
@@ -2,11 +2,21 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @Bean
+    OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails resource) {
+        return new OAuth2RestTemplate(resource);
     }
 }
